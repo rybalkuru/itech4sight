82cf520d5a15e13807dd8ef2bd2e9b02b03bddda
Сергей Безденежных <rybalkuru@yandex.ru>
2025-08-12 17:36:03 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,139 +1,126 @@
 const gulp = require("gulp");
-const newer = require("gulp-newer");
-const concat = require("gulp-concat");
+const dartSass = require("sass");
+const gulpSass = require("gulp-sass");
 const cleanCSS = require("gulp-clean-css");
-const uglify = require("gulp-uglify-es").default;
-const sass = require("gulp-sass")(require("sass"));
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const rename = require("gulp-rename");
-const plumber = require("gulp-plumber");
 const del = require("del");
+const rename = require("gulp-rename");
 const replace = require("gulp-replace");
+const browserSync = require("browser-sync").create();
 
-// Путь для замены
-const SITE_TEMPLATE_PATH = "SITE_TEMPLATE_PATH";
+const rollupStream = require("@rollup/stream");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const terser = require("gulp-terser");
+const resolve = require("@rollup/plugin-node-resolve").default;
+const commonjs = require("@rollup/plugin-commonjs");
 
-const paths = {
-    src: {
-        root: "src",
-        scss: "src/scss/**/*.scss",
-        js: "src/js/**/*.js",
-        images: "src/images/**/*",
-        fonts: "src/fonts/**/*",
-        videos: "src/videos/**/*",
-        html: "src/**/*.html",
-    },
-    dist: {
-        root: "dist",
-        css: "dist/css",
-        js: "dist/js",
-        images: "dist/images",
-        fonts: "dist/fonts",
-        videos: "dist/videos",
-    },
-};
+const sass = gulpSass(dartSass);
 
+// Очистка dist
 function clean() {
-    return del(paths.dist.root);
+    return del(["dist"]);
 }
 
+// Компиляция SCSS → CSS в dist/css
 function styles() {
     return gulp
-        .src(paths.src.scss, { sourcemaps: true })
-        .pipe(plumber())
+        .src("src/scss/main.scss")
         .pipe(sass().on("error", sass.logError))
-        .pipe(postcss([autoprefixer()]))
+        .pipe(
+            postcss([
+                autoprefixer({
+                    overrideBrowserslist: [
+                        "last 2 versions",
+                        "> 1%",
+                        "not dead",
+                    ],
+                    cascade: false,
+                }),
+            ])
+        )
         .pipe(cleanCSS())
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest(paths.dist.css, { sourcemaps: "." }));
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
 }
 
+// Сборка JS с Rollup + минификация в dist/js
 function scripts() {
-    return gulp
-        .src(paths.src.js, { sourcemaps: true })
-        .pipe(plumber())
-        .pipe(concat("main.min.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.dist.js, { sourcemaps: "." }));
+    return rollupStream({
+        input: "src/js/main.js",
+        output: {
+            file: "main.js",
+            format: "iife",
+            sourcemap: true,
+            name: "bundle",
+        },
+        plugins: [resolve(), commonjs()],
+    })
+        .pipe(source("main.js"))
+        .pipe(buffer())
+        .pipe(terser())
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
 }
 
-function images() {
+
+// Обработка HTML — замена путей и копирование в dist
+function html() {
     return gulp
-        .src(paths.src.images)
-        .pipe(newer(paths.dist.images))
-        .pipe(gulp.dest(paths.dist.images));
+        .src("src/**/*.html")
+        .pipe(replace(/dist\//g, "<?= SITE_TEMPLATE_PATH ?>/"))
+        .pipe(gulp.dest("dist"));
 }
 
+// Копирование ассетов
+function assets() {
+    return gulp
+        .src(["src/**/*.{jpg,jpeg,png,svg,gif,webp,mp4,webm}"])
+        .pipe(gulp.dest("dist"));
+}
+
+// Копирование шрифтов
 function fonts() {
     return gulp
-        .src(paths.src.fonts)
-        .pipe(newer(paths.dist.fonts))
-        .pipe(gulp.dest(paths.dist.fonts));
+        .src("src/fonts/**/*.{woff,woff2,ttf,eot,otf}")
+        .pipe(gulp.dest("dist/fonts"));
 }
 
-function videos() {
-    return gulp
-        .src(paths.src.videos)
-        .pipe(newer(paths.dist.videos))
-        .pipe(gulp.dest(paths.dist.videos));
+// Сервер и слежение
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "dist",
+        },
+        port: 3000,
+        notify: false,
+    });
+
+    gulp.watch("src/scss/**/*.scss", styles);
+    gulp.watch("src/js/**/*.js", scripts);
+    gulp.watch("src/**/*.html", html).on("change", browserSync.reload);
+    gulp.watch("src/fonts/**/*.{woff,woff2,ttf,eot,otf}", fonts).on(
+        "change",
+        browserSync.reload
+    );
+    gulp.watch("src/**/*.{jpg,jpeg,png,svg,gif,webp,mp4,webm}", assets).on(
+        "change",
+        browserSync.reload
+    );
 }
 
-function html() {
-    return gulp.src(paths.src.html).pipe(gulp.dest(paths.dist.root));
-}
-
-// Задача замены путей в CSS
-function replaceCssPaths() {
-    return gulp
-        .src(paths.dist.css + "/**/*.css")
-        .pipe(
-            replace(
-                /url\((['"]?)(?!https?:|\/\/|data:)([^'")]+)\1\)/g,
-                (match, quote, url) => {
-                    return `url(${quote}${SITE_TEMPLATE_PATH}/${url}${quote})`;
-                }
-            )
-        )
-        .pipe(gulp.dest(paths.dist.css));
-}
-
-// Задача замены путей в JS
-function replaceJsPaths() {
-    return gulp
-        .src(paths.dist.js + "/**/*.js")
-        .pipe(
-            replace(
-                /(['"])(\/?(images|fonts|videos|css|js)[^'"]*)\1/g,
-                (match, quote, url) => {
-                    return `${quote}${SITE_TEMPLATE_PATH}/${url.replace(
-                        /^\/?/,
-                        ""
-                    )}${quote}`;
-                }
-            )
-        )
-        .pipe(gulp.dest(paths.dist.js));
-}
-
-// Объединённая задача замены путей
-const replacePaths = gulp.parallel(replaceCssPaths, replaceJsPaths);
-
-// Основная сборка
 const build = gulp.series(
     clean,
-    gulp.parallel(styles, scripts, images, fonts, videos, html),
-    replacePaths
+    gulp.parallel(styles, scripts, html, assets, fonts)
 );
+const dev = gulp.series(build, serve);
 
-// Экспорт задач
 exports.clean = clean;
 exports.styles = styles;
 exports.scripts = scripts;
-exports.images = images;
-exports.fonts = fonts;
-exports.videos = videos;
 exports.html = html;
-exports.replacePaths = replacePaths;
+exports.assets = assets;
+exports.fonts = fonts;
 exports.build = build;
-exports.default = build;
+exports.serve = serve;
+exports.dev = dev;
+exports.default = dev;
